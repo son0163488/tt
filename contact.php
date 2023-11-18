1bcdf15772de04bca51ff65bc506f24abdc6f427
son0163488 <nguyenvanson112003hh1@gmail.com>
2023-11-19 01:28:43 +0700

    update

---
+++ contact.php
@@ -0,0 +1,47 @@
+<?php 
+	include "head.php"
+	?>
+<?php
+$title ="Shop Son";
+$name ="Book";
+?>
+<?php 
+	include "top.php"
+    ?>
+    <?php 
+	include "Header.php"
+	?>
+	<?php 
+	include "navigation.php"
+	?>
+	<!--//////////////////////////////////////////////////-->
+	<!--///////////////////Contact Page///////////////////-->
+	<!--//////////////////////////////////////////////////-->
+	<div id="page-content" class="single-page">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading"><h1>Liên hệ với chúng tôi</h1></div>
+				</div>
+				<div class="col-md-6">
+					<p><span class="glyphicon glyphicon-home"></span>Tòa nhà FPT Polytechnic., Cổng số 2, 13 P. Trịnh Văn Bô, Xuân Phương, Nam Từ Liêm, Hà Nội, Vietnam</p>
+					<p><span class="glyphicon glyphicon-earphone"></span> 012345678</p>
+					<p><span class="glyphicon glyphicon-envelope"></span>  info.sonnv@gmail.com</p>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8850392357813!2d105.74650027500067!3d21.037285387486207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313455e940879933%3A0xcf10b34e9f1a03df!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1sen!2s!4v1700330426212!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php 
+	include "footer.php"
+	?>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,16 @@
+
+	<footer>
+		<div class="container">
+			<div class="wrap-footer">
+				<div class="row">
+					<div class="col-md-3 col-footer footer-1">
+						<h5>Nhà sách online</h5>
+						<p>Nhà sách online với rất nhiều đầu sách hấp dẫn cho bạn đọc lựa chọn.</p>
+					</div>
+					</div>
+					<p><span class="glyphicon glyphicon-home"></span>Hà Nội, Việt Nam.</p>
+					
+			</div>
+		</div>
+		
+	</footer>
